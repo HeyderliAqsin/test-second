7538ee665243f6fb332d7a9a30e749547ec168da
Aqsin <heyderliaqsin94@gmail.com>
2021-08-31 14:01:17 +0400

    test 2ci

--- js/app.js
+++ js/app.js
@@ -10,3 +10,42 @@ for(let i=0;i<tabBtn.length;i++){
       pTag[btnIndex].classList.add("tab-active")
    }
 }
+
+
+var Reqemler=[10,68,987,7,1,8]
+
+let maxReqem = Reqemler[0]
+for (let i=0;i<Reqemler.length;i++){
+    if(maxReqem<Reqemler[i]){
+        maxReqem=Reqemler[i]
+    }
+}
+console.log(maxReqem);
+
+
+var change=function convert(olcu) {
+    var x;
+    if (olcu == "KM") {
+      x = document.getElementById("km").value * 1000;
+    } else {
+     
+    
+    }
+  }
+  change()
+ 
+
+
+function myRand(){
+let myRandom=Math.floor(Math.random()*100)
+let count=0
+
+while(myRandom !=30){
+    myRandom=Math.floor(Math.random()*100)
+    if(myRandom==30){
+        break;
+    }
+    count++
+}
+}
+myRand()
